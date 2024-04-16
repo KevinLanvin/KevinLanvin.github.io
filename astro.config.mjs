ed4804ef5ee69b9d650577bb49c2f3dba57b05a7
KevinLanvin <info.lanvink@gmail.com>
2024-04-16 11:50:38 +0200

    Add compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [playformCompress()]
 });
