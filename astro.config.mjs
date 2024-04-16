5ded468476c7eb51ce7f6a7af219e6eea2a530c7
KevinLanvin <info.lanvink@gmail.com>
2024-04-16 12:10:08 +0200

    Remove fucking deep shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-import playformCompress from "@playform/compress";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [playformCompress()]
 });
